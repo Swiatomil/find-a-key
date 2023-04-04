f4a8ac2e3fccd5981137547c0d0d58f5eab59b0c
Swiatomil <bartoszarent@gmail.com>
2023-04-04 16:45:51 +0200

    basic but working

--- find-a-key.py
+++ find-a-key.py
@@ -7,6 +7,7 @@ class game:
         self.map[self.x][self.y]="5"
         self.keyx=randint(0,row-1)
         self.keyy=randint(0,column-1)
+        self.distance = abs(self.x-self.keyx) + abs(self.y-self.keyy)
         while self.map[self.keyx][self.keyy]=="5":
             self.keyx=randint(0,row-1)
             self.keyy=randint(0,column-1)
@@ -59,12 +60,21 @@ class game:
                 print(self)  
         if self.x==self.keyx and self.y==self.keyy:
             print('you win!') 
-            return 0                     
+            return 0
+    def give_clue(self):
+        new_distance = abs(self.x-self.keyx) + abs(self.y-self.keyy)
+        if new_distance< self.distance:
+            print("hotter")
+            self.distance=new_distance
+        if new_distance > self.distance:
+            print("colder")
+            self.distance=new_distance                     
         
 
-b=game()               
+b=game(5,5)               
 print(b)
 c="1"
 while(c!=0):
     
     c = b.move(input())
+    b.give_clue()
